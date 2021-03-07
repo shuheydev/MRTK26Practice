5094d173ceae85e8f5b1e57dd1e41b1e46a12d3a
shuhei.nishizawa <shuheydev@gmail.com>
2021-03-07 11:26:11 +0900

    initial commit

---
+++ Assets/App/Scripts/ChangeLocalGrabity.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeLocalGrabity : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 localGravity;
+    private Rigidbody rBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rBody = this.GetComponent<Rigidbody>();
+        rBody.useGravity = false;
+    }
+
+    private void FixedUpdate()
+    {
+        SetLocalGravity();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void SetLocalGravity()
+    {
+        rBody.AddForce(localGravity, ForceMode.Acceleration);
+    }
+
+    public void SetIsKinematics(bool isKinematics)
+    {
+        rBody.isKinematic = isKinematics;
+    }
+}
